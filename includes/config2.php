547a24fcd155fe29f2330a835c50ef2e955503f9
san-s <42447997+san-s@users.noreply.github.com>
2023-03-27 23:20:41 +0100

    Create config2.php

---
+++ includes/config2.php
@@ -0,0 +1,16 @@
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:elibrary1.database.windows.net,1433; Database = elibrary", "CloudSAd6857fe5", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "CloudSAd6857fe5", "pwd" => "{your_password_here}", "Database" => "elibrary", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:elibrary1.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
