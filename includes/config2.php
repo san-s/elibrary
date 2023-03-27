3f6b61d3e450f3e312091e202f8c30f3dd3551e8
san-s <42447997+san-s@users.noreply.github.com>
2023-03-28 00:49:11 +0100

    Update config2.php

--- includes/config2.php
+++ includes/config2.php
@@ -1,16 +1,5 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:elibrary1.database.windows.net,1433; Database = elibrary", "CloudSAd6857fe5", "Test@123");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "CloudSAd6857fe5", "pwd" => "{your_password_here}", "Database" => "elibrary", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:elibrary1.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
+mysqli_real_connect($conn, "elibrary1.mysql.database.azure.com", "CloudSAd6857fe5", "Test@123", "{your_database}", 3306, MYSQLI_CLIENT_SSL);
 ?>
